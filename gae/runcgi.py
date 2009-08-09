3529a2ce97ee42a3abab4a0bc6a2a092fd074fda
lericson <ludvig@lericson.se>
2009-08-09 20:53:14 +0200

    Flexibilize imports of zip packages.

--- gae/runcgi.py
+++ gae/runcgi.py
@@ -3,7 +3,10 @@ import os
 from glob import glob
 zip_packs = glob(os.path.join(os.path.dirname(__file__), "lib", "*.zip"))
 sys.path[:] = zip_packs + sys.path
-import werkzeug, jinja2, simplejson
+for zip_pack_fn in zip_packs:
+    modname = os.path.basename(zip_pack_fn)
+    modname = os.path.splitext(modname)[0]
+    __import__(modname, fromlist=[""])
 
 def main():
     from wsgiref.handlers import CGIHandler
